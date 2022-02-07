815e17a528391a70ebc6355635106d077d7af82d
Vinicius Justino <visilva5690@hotmail.com>
2022-02-07 09:02:22 -0300

    Implementing possible moves of rook

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -25,7 +25,7 @@ public class Board {
 	}
 
 	public Piece piece(int row, int column) {
-		if(!positionExists(row, column)) {
+		if (!positionExists(row, column)) {
 			throw new BoardException("Position not on the board!");
 		}
 		return pieces[row][column];
@@ -34,27 +34,41 @@ public class Board {
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColunm()];
 	}
-	
+
 	public void placePiece(Piece piece, Position position) {
-		if(thereIsAPiece(position))
+		if (thereIsAPiece(position))
 			throw new BoardException("There is a already a piece " + position);
 		pieces[position.getRow()][position.getColunm()] = piece;
 		piece.position = position;
 	}
+
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board! ");
+		}
+		
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColunm()] = null;
+		return aux;
+	}
 	
+
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < colunms;
 	}
-	
+
 	public boolean positionExists(Position position) {
 		return positionExists(position.getRow(), position.getColunm());
 	}
-	
+
 	public boolean thereIsAPiece(Position position) {
-		if(!positionExists(position)) {
+		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board!");
 		}
 		return piece(position) != null;
 	}
 }
-
--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -4,20 +4,20 @@ import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece {
-
-	public King(Board board, Color color) {
+public class Bishop extends ChessPiece{
+	public Bishop(Board board, Color color) {
 		super(board, color);
 	}
-
+	
 	@Override
 	public String toString() {
-		return "K";
+		return "B";
+		
 	}
 
 	@Override
 	public boolean[][] possibleMoves() {
-		boolean [][] mat = new boolean [getBoard().getRows()][getBoard().getColunms()];
-		return mat;
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
